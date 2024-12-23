5eddb1adc4e51653db6b1e5328f53182911de3fc
Nishil Adina <162916939+nishiladina@users.noreply.github.com>
2024-12-22 20:22:38 -0500

    Update WiFiHeaders.h

--- WiFiHeaders.h
+++ WiFiHeaders.h
@@ -2,16 +2,9 @@
 #include <WiFiServer.h>
 #include "OpenThingsFramework.h"
 #include "ui_html.h"
-#include "style_css.h"
 #include <iostream>
 #include <string>
 
-
-#define WIFI_SSID "CICS_Makerspace"
-#define WIFI_PASSWORD "makerspace"
 #define CLOUD_HOST "ws.cloud.openthings.io"
 #define CLOUD_PORT 80
 #define LOCAL_SERVER_PORT 80
-
-// !!! Replace this by the unique token created for you !!!
-#define CLOUD_TOKEN "OT4fa175d0cfd69f1457b8dba3d4d175"
