27a6876bef7f57ad396d6c4c98a5eb04969461ff
Nishil Adina <162916939+nishiladina@users.noreply.github.com>
2024-12-22 20:22:15 -0500

    Update MotorHeaders.h

--- MotorHeaders.h
+++ MotorHeaders.h
@@ -1,10 +1,7 @@
 #include <AccelStepper.h>
 #include <Adafruit_NeoPixel.h>
 
-#define NEOP_PIN 25
-
-// do not change these
-#define ESTOP 15 // not needed
+// do not change these - see wiring guide
 #define ASTEP 13
 #define ADIR 12
 #define BSTEP 14
@@ -15,18 +12,20 @@
 #define TOP_BUTTON 34
 #define MIDDLE_BUTTON 0
 #define BOTTOM_BUTTON 35
+#define NEOP_PIN 25
 
-#define STEPS_PER_ROTATION 800
 #define LOWERED_POSITION 10 * STEPS_PER_ROTATION
+#define FREE_FLOW_RAISED_POSITION -3 * STEPS_PER_ROTATION
 #define RAISED_POSITION -4 * STEPS_PER_ROTATION
 
-// change these depending on what motor gets plugged into A vs B vs C
-#define XSTEP CSTEP
-#define XDIR CDIR
-#define Z1STEP ASTEP
-#define Z1DIR ADIR
-#define Z2STEP BSTEP
-#define Z2DIR BDIR
+#define DRINK1 -750 // rightmost drink
+#define DRINK2 -4000
+#define DRINK3 -7250
+#define DRINK4 -10700 // leftmost drink
 
+// function prototypes - need one for each drink
 void makeGrinchPunch();
 void makeShirleyTemple();
+void makeSprite();
+void makeGingerAle();
+void makeHawaiianPunch();
